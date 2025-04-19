85566759fc6e1f67c6a919b859b8331246b056e6
erasmurplus <osgmur@outlook.com>
2025-04-19 22:44:33 +0300

    a_.py dosyası güncellendi.

--- a_.py
+++ a_.py
@@ -1,6 +1,1 @@
 print("deneme")
-
-
-(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ git config --global user.name "erasmurplus"
-(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ git config --global user.email "osgmur@outlook.com"
-(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ 
