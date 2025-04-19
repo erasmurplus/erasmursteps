e53eb35adfb4e6554fedf36c4057dfc1c9c14a8e
erasmurplus <osgmur@outlook.com>
2025-04-19 22:33:51 +0300

    git dizini oluşturuldu

--- a_.py
+++ a_.py
@@ -1,1 +1,6 @@
 print("deneme")
+
+
+(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ git config --global user.name "erasmurplus"
+(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ git config --global user.email "osgmur@outlook.com"
+(.ersmrVENV) .ersmrVENV[fedmur@fedora erasmurplus]$ 
